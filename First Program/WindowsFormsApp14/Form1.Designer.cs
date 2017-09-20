7ec796087aceeb995b7e3449803125329a9d679a
piterek20000 <31956507+piterek20000@users.noreply.github.com>
2017-09-20 19:51:01 +0200

    Add files via upload

---
+++ First Program/WindowsFormsApp14/Form1.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace WindowsFormsApp14
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FirstNameTextBox = new System.Windows.Forms.TextBox();
+            this.LastNameTextBox = new System.Windows.Forms.TextBox();
+            this.AgeTextBox = new System.Windows.Forms.TextBox();
+            this.BirthdayTextBox = new System.Windows.Forms.TextBox();
+            this.AdressTextBox = new System.Windows.Forms.TextBox();
+            this.PhoneTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ConfirmButton = new System.Windows.Forms.Button();
+            this.ClientsListView = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader6 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // FirstNameTextBox
+            // 
+            this.FirstNameTextBox.BackColor = System.Drawing.Color.Purple;
+            this.FirstNameTextBox.Location = new System.Drawing.Point(199, 56);
+            this.FirstNameTextBox.Name = "FirstNameTextBox";
+            this.FirstNameTextBox.Size = new System.Drawing.Size(252, 35);
+            this.FirstNameTextBox.TabIndex = 0;
+            this.FirstNameTextBox.MouseLeave += new System.EventHandler(this.FirstNameTextBox_MouseLeave);
+            this.FirstNameTextBox.MouseHover += new System.EventHandler(this.FirstNameTextBox_MouseHover);
+            // 
+            // LastNameTextBox
+            // 
+            this.LastNameTextBox.BackColor = System.Drawing.Color.Purple;
+            this.LastNameTextBox.Location = new System.Drawing.Point(199, 97);
+            this.LastNameTextBox.Name = "LastNameTextBox";
+            this.LastNameTextBox.Size = new System.Drawing.Size(252, 35);
+            this.LastNameTextBox.TabIndex = 0;
+            this.LastNameTextBox.MouseLeave += new System.EventHandler(this.LastNameTextBox_MouseLeave);
+            this.LastNameTextBox.MouseHover += new System.EventHandler(this.LastNameTextBox_MouseHover);
+            // 
+            // AgeTextBox
+            // 
+            this.AgeTextBox.BackColor = System.Drawing.Color.Purple;
+            this.AgeTextBox.Location = new System.Drawing.Point(199, 138);
+            this.AgeTextBox.Name = "AgeTextBox";
+            this.AgeTextBox.Size = new System.Drawing.Size(252, 35);
+            this.AgeTextBox.TabIndex = 0;
+            this.AgeTextBox.MouseLeave += new System.EventHandler(this.AgeTextBox_MouseLeave);
+            this.AgeTextBox.MouseHover += new System.EventHandler(this.AgeTextBox_MouseHover);
+            // 
+            // BirthdayTextBox
+            // 
+            this.BirthdayTextBox.BackColor = System.Drawing.Color.Purple;
+            this.BirthdayTextBox.Location = new System.Drawing.Point(199, 179);
+            this.BirthdayTextBox.Name = "BirthdayTextBox";
+            this.BirthdayTextBox.Size = new System.Drawing.Size(252, 35);
+            this.BirthdayTextBox.TabIndex = 0;
+            this.BirthdayTextBox.MouseLeave += new System.EventHandler(this.BirthdayTextBox_MouseLeave);
+            this.BirthdayTextBox.MouseHover += new System.EventHandler(this.BirthdayTextBox_MouseHover);
+            // 
+            // AdressTextBox
+            // 
+            this.AdressTextBox.BackColor = System.Drawing.Color.Purple;
+            this.AdressTextBox.Location = new System.Drawing.Point(199, 220);
+            this.AdressTextBox.Name = "AdressTextBox";
+            this.AdressTextBox.Size = new System.Drawing.Size(252, 35);
+            this.AdressTextBox.TabIndex = 0;
+            this.AdressTextBox.MouseLeave += new System.EventHandler(this.AdressTextBox_MouseLeave);
+            this.AdressTextBox.MouseHover += new System.EventHandler(this.AdressTextBox_MouseHover);
+            // 
+            // PhoneTextBox
+            // 
+            this.PhoneTextBox.BackColor = System.Drawing.Color.Purple;
+            this.PhoneTextBox.Location = new System.Drawing.Point(199, 261);
+            this.PhoneTextBox.Name = "PhoneTextBox";
+            this.PhoneTextBox.Size = new System.Drawing.Size(252, 35);
+            this.PhoneTextBox.TabIndex = 0;
+            this.PhoneTextBox.MouseLeave += new System.EventHandler(this.PhoneTextBox_MouseLeave);
+            this.PhoneTextBox.MouseHover += new System.EventHandler(this.PhoneTextBox_MouseHover);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(52, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(130, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 29);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Age";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(78, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(115, 29);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Birthday ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(95, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 29);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Adress";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(101, 264);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 29);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Phone";
+            // 
+            // ConfirmButton
+            // 
+            this.ConfirmButton.AutoSize = true;
+            this.ConfirmButton.BackColor = System.Drawing.Color.Green;
+            this.ConfirmButton.ForeColor = System.Drawing.Color.Lime;
+            this.ConfirmButton.Location = new System.Drawing.Point(79, 321);
+            this.ConfirmButton.Name = "ConfirmButton";
+            this.ConfirmButton.Size = new System.Drawing.Size(114, 39);
+            this.ConfirmButton.TabIndex = 2;
+            this.ConfirmButton.Text = "Confirm";
+            this.ConfirmButton.UseVisualStyleBackColor = false;
+            this.ConfirmButton.Click += new System.EventHandler(this.ConfirmButton_Click);
+            // 
+            // ClientsListView
+            // 
+            this.ClientsListView.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientsListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5,
+            this.columnHeader6});
+            this.ClientsListView.Location = new System.Drawing.Point(457, 56);
+            this.ClientsListView.Name = "ClientsListView";
+            this.ClientsListView.Size = new System.Drawing.Size(775, 460);
+            this.ClientsListView.TabIndex = 3;
+            this.ClientsListView.UseCompatibleStateImageBehavior = false;
+            this.ClientsListView.View = System.Windows.Forms.View.Details;
+            this.ClientsListView.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.ClientsListView_ColumnClick);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "FIrst Name";
+            this.columnHeader1.Width = 152;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Last Name";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader2.Width = 147;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Age";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader3.Width = 72;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Birthday";
+            this.columnHeader4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader4.Width = 123;
+            // 
+            // columnHeader5
+            // 
+            this.columnHeader5.Text = "Adress";
+            this.columnHeader5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader5.Width = 106;
+            // 
+            // columnHeader6
+            // 
+            this.columnHeader6.Text = "Phone";
+            this.columnHeader6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.columnHeader6.Width = 163;
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.AutoSize = true;
+            this.DeleteButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.DeleteButton.ForeColor = System.Drawing.Color.Yellow;
+            this.DeleteButton.Location = new System.Drawing.Point(199, 321);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(109, 39);
+            this.DeleteButton.TabIndex = 4;
+            this.DeleteButton.Text = "Delete";
+            this.DeleteButton.UseVisualStyleBackColor = false;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.Indigo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1237, 556);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.ClientsListView);
+            this.Controls.Add(this.ConfirmButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.PhoneTextBox);
+            this.Controls.Add(this.AdressTextBox);
+            this.Controls.Add(this.BirthdayTextBox);
+            this.Controls.Add(this.AgeTextBox);
+            this.Controls.Add(this.LastNameTextBox);
+            this.Controls.Add(this.FirstNameTextBox);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.ForeColor = System.Drawing.Color.Yellow;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main Panel";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox FirstNameTextBox;
+        private System.Windows.Forms.TextBox LastNameTextBox;
+        private System.Windows.Forms.TextBox AgeTextBox;
+        private System.Windows.Forms.TextBox BirthdayTextBox;
+        private System.Windows.Forms.TextBox AdressTextBox;
+        private System.Windows.Forms.TextBox PhoneTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button ConfirmButton;
+        private System.Windows.Forms.ListView ClientsListView;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ColumnHeader columnHeader6;
+        private System.Windows.Forms.Button DeleteButton;
+    }
+}
+
---
+++ First Program/WindowsFormsApp14/Zabawa w WindowsApps.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp14
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region MouseHoverTextBox
+        private void FirstNameTextBox_MouseHover(object sender, EventArgs e)
+        {
+            FirstNameTextBox.BackColor = Color.ForestGreen;
+        }
+
+        private void LastNameTextBox_MouseHover(object sender, EventArgs e)
+        {
+            LastNameTextBox.BackColor = Color.ForestGreen;
+        }
+
+        private void AgeTextBox_MouseHover(object sender, EventArgs e)
+        {
+            AgeTextBox.BackColor = Color.ForestGreen;
+        }
+
+        private void BirthdayTextBox_MouseHover(object sender, EventArgs e)
+        {
+            BirthdayTextBox.BackColor = Color.ForestGreen;
+        }
+
+        private void AdressTextBox_MouseHover(object sender, EventArgs e)
+        {
+            AdressTextBox.BackColor = Color.ForestGreen;
+        }
+
+        private void PhoneTextBox_MouseHover(object sender, EventArgs e)
+        {
+            PhoneTextBox.BackColor = Color.ForestGreen;
+        }
+        #endregion
+
+        #region MouseLeaveTextBox
+        private void FirstNameTextBox_MouseLeave(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(FirstNameTextBox.Text))
+                FirstNameTextBox.BackColor = Color.LightGreen;
+            else
+                FirstNameTextBox.BackColor = Color.Purple;
+        }
+
+        private void LastNameTextBox_MouseLeave(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(LastNameTextBox.Text))
+                LastNameTextBox.BackColor = Color.LightGreen;
+            else
+                LastNameTextBox.BackColor = Color.Purple;
+        }
+
+        private void AgeTextBox_MouseLeave(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(AgeTextBox.Text))
+                AgeTextBox.BackColor = Color.LightGreen;
+            else
+                AgeTextBox.BackColor = Color.Purple;
+        }
+
+        private void BirthdayTextBox_MouseLeave(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(BirthdayTextBox.Text))
+                BirthdayTextBox.BackColor = Color.LightGreen;
+            else
+                BirthdayTextBox.BackColor = Color.Purple;
+        }
+
+        private void AdressTextBox_MouseLeave(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(AdressTextBox.Text))
+                AdressTextBox.BackColor = Color.LightGreen;
+            else
+                AdressTextBox.BackColor = Color.Purple;
+        }
+
+        private void PhoneTextBox_MouseLeave(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(PhoneTextBox.Text))
+                PhoneTextBox.BackColor = Color.LightGreen;
+            else
+                PhoneTextBox.BackColor = Color.Purple;
+        }
+
+
+
+        #endregion
+
+        private void ConfirmButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(FirstNameTextBox.Text))
+                FirstNameTextBox.BackColor = Color.Red;
+            if (string.IsNullOrEmpty(LastNameTextBox.Text))
+                LastNameTextBox.BackColor = Color.Red;
+            if (string.IsNullOrEmpty(AgeTextBox.Text))
+                AgeTextBox.BackColor = Color.Red;
+            if (string.IsNullOrEmpty(BirthdayTextBox.Text))
+                BirthdayTextBox.BackColor = Color.Red;
+            if (string.IsNullOrEmpty(AdressTextBox.Text))
+                AdressTextBox.BackColor = Color.Red;
+            if (string.IsNullOrEmpty(PhoneTextBox.Text))
+                PhoneTextBox.BackColor = Color.Red;
+
+            else
+            {
+                ListViewItem A = new ListViewItem();
+                A.Text = FirstNameTextBox.Text;
+                A.SubItems.Add(LastNameTextBox.Text);
+                A.SubItems.Add(AgeTextBox.Text);
+                A.SubItems.Add(BirthdayTextBox.Text);
+                A.SubItems.Add(AdressTextBox.Text);
+                A.SubItems.Add(PhoneTextBox.Text);
+
+                ClientsListView.Items.Add(A);
+            }
+        }
+
+        private void ClientsListView_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem eachItem in ClientsListView.SelectedItems)
+            {
+                ClientsListView.Items.Remove(eachItem);
+            }
+        }
+    }
+}
